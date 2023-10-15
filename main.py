4b1fe5eadb803487f69ea76ab0c2e34873256229
Tqgd <oxbxcubi@gmail.com>
2023-10-15 14:01:14 -0700

    Beta v0.2 Translations QoL

--- main.py
+++ main.py
@@ -19,20 +19,24 @@ while rep_point < point_count:
 
     rep_point = rep_point + 1
 
-# Shifts x "h" units to the left/right
-h = int(input("Horizontal Shift: "))
-
-# Shifts y "k" units up/down
-k = int(input("Vertical Shift: "))
-
-# Convert str to int
-# Apply translation
-
+# Evaluate list
 x_list = [eval(i) for i in x_list_str]
-x_list_h = [x + h for x in x_list]
 
 y_list = [eval(i) for i in y_list_str]
-y_list_k = [y + k for y in y_list]
+
+# Shifts x "h" units to the left/right
+h = (input("Horizontal Shift: "))
+h = eval(h)
+
+# Shifts y "k" units up/down
+k = (input("Vertical Shift: "))
+k = eval(k)
+
+# Apply translation
+
+x_list_h = [round(x + h, 3) for x in x_list]
+
+y_list_k = [round(y + k, 3) for y in y_list]
 
 # Make ordered pairs
 
